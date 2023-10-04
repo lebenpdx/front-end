d9d8690fa4302b9516f62d4ddc0f96205e832799
lebenpdx <116242222+lebenpdx@users.noreply.github.com>
2023-10-03 18:07:41 -0700

    completed hw1

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,19 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("input", () => {
+  let isPal = false;
+  if (elem.value > 0) {
+    const NumtoStr = elem.value.toString();
+    isPal = NumtoStr === NumtoStr.split("").reverse().join("");
+  }
+
+  const result = document.querySelector("div:last-child div");
+
+  if (isPal) {
+    result.textContent = "Yes. This is a palindrome!";
+    result.style.color = "green";
+  } else {
+    result.textContent = "No. This is NOT a palindrome.";
+    result.style.color = "red";
+  }
+});
