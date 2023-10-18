bb8d90938368f82a38e471a6738f3ea6d641e45a
lebenpdx <116242222+lebenpdx@users.noreply.github.com>
2023-10-18 15:37:17 -0700

    completed hw2

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,18 @@
-// Add your code here
+document.getElementById('form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const fullName = document.getElementById('fullName').value;
+  const email = document.getElementById('email').value;
+  const registrationStatus =
+    document.getElementById('registrationStatus').value;
+  const courses = Array.from(
+    document.querySelectorAll('input[name="courses"]:checked')
+  ).map((c) => c.value);
+  const anythingElse = document.getElementById('anythingElse').value;
+
+  console.log('Full Name: ' + fullName);
+  console.log('Email: ' + email);
+  console.log('Registration Status: ' + registrationStatus);
+  console.log('Courses: ' + courses.join(', '));
+  console.log('Anything Else: ' + anythingElse);
+});
